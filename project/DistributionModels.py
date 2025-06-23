d455d4ce23d5c0e78b12c842b50f3aec1361c0bf
adaree1112 <adaree1112@highgateschool.org.uk>
2025-06-23 15:01:46 +0000

    piecewise stuff

--- project/DistributionModels.py
+++ project/DistributionModels.py
@@ -1,4 +1,5 @@
 from scipy.stats import norm, binom, geom
+from sympy import sympify, symbols
 
 class DistributionModel:
     def __init__(self, distribution,args):
@@ -13,14 +14,28 @@ class DistributionModel:
     def cumulative_probability(self,number):
         return self.distribution.cdf(number)
 
-# class InputtedPDFDistributionModel:
-#     def __init__(self, pieces : dict):
-#         self.pieces = sorted(pieces.values(), key=lambda x: x[1])
-#         print(pieces)
+class InputtedPDFDistributionModel:
+    def __init__(self, mini, pieces : dict):
+        self.pieces = {k: v for k, v in sorted(pieces.items(), key=lambda item: item[1])}
+        self.mini = mini
+        print(self.pieces)
 
+    # def probability(self,number):
+    #     x = symbols("x")
+    #
+    #     if number < self.mini:
+    #         return 0
+    #     for piece in self.pieces:
+    #         equation = sympify(piece[0])
+    #         maxi = piece[1]
+    #         if number < maxi:
+    #             return equation.subs(x,number)
 
 if __name__ == "__main__":
     # X = DistributionModel("G",[0.25])
     # print(X.probability(2))
 
-    # X = InputtedPDFDistributionModel({"(1-x)**2":1, "x**2":0.5})
+    X = InputtedPDFDistributionModel(0,{"0.5x": 1, "1-0.5x": 2})
+    print(X.probability(0))
+    print(X.probability(1))
+    print(X.probability(2))
