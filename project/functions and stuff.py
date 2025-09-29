123feb7b3a662769a925738fcdbcce567f055949
adaree1112 <adaree1112@highgateschool.org.uk>
2025-09-29 14:58:38 +0000

    normalisation

--- project/functions and stuff.py
+++ project/functions and stuff.py
@@ -2,38 +2,31 @@
 def piecewise_linear (points):
     output = []
     for point,nextpoint in zip(points,points[1:]):
-        x1=point[0]
-        y1=point[1]
-        x2=nextpoint[0]
-        y2=nextpoint[1]
+        if point == nextpoint:
+            continue
+        x1,y1=point
+        x2,y2=nextpoint
         m=(y2-y1)/(x2-x1)
         c=y1-m*x1
         output.append((point[0],nextpoint[0],m,c))
     return output
 
 def normalise_piecewise_linear (pieces):
-    output = []
     area=0
     for piece in pieces:
-        x1=piece[0]
-        x2=piece[1]
-        m=piece[2]
-        c=piece[3]
+        x1,x2,m,c=piece
+
         y1=m*x1+c
         y2=m*x2+c
+
         trapeziumarea = .5*(y1+y2)*(x2-x1)
         area+=trapeziumarea
 
     areascalefactor=1/area
-    lengthscalefactor=areascalefactor**.5
-    print(area,areascalefactor,lengthscalefactor)
 
     points=[]
     for piece in pieces:
-        x1=piece[0]
-        x2=piece[1]
-        m=piece[2]
-        c=piece[3]
+        x1,x2,m,c=piece
 
         y1=m*x1+c
         y2=m*x2+c
@@ -47,8 +40,9 @@ def normalise_piecewise_linear (pieces):
     output=piecewise_linear(points)
     return output
 
-print(pieces:=piecewise_linear([(1, 1), (2, 2), (3, 1)]))
+def piecewise_cubic(points):
+    pass
 
 
-
+print(pieces:=piecewise_linear([(1, 1), (2, 2), (3,1)]))
 print(normalise_piecewise_linear(pieces))
