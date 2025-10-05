503dfa71f8696fc7501f91d15b3590417b65e9ca
adam <adaree1112@highgateschool.org.uk>
2025-10-05 19:16:04 +0100

    cubic splines neatened

--- project/piecewisecubicsplines.py
+++ project/piecewisecubicsplines.py
@@ -60,6 +60,46 @@ def piecewise_cubic_spline(points):
             for i in range(n)]
     return pieces
 
+def piecewise_cubic_spline(points):
+
+    points = sorted(points, key=lambda p: p[0])
+    print(points)
+    n=len(points)-1
+
+    matrix=np.zeros((4*n,4*n))
+    matb=np.zeros((4*n,1))
+
+    ## f(X1) = y1, f(X2)=y2
+    for i in range(n):
+        matrix[2*i][4*i:4*i+4] = [points[i][0]**3,points[i][0]**2,points[i][0],1]
+        matb[2*i][0]=points[i][1]
+
+        matrix[2*i+1][4*i:4*i+4] = [points[i+1][0]**3,points[i+1][0]**2,points[i+1][0],1]
+        matb[2*i+1][0] = points[i+1][1]
+
+    ## f1'(x2)-f2'(x2)=0
+    for i in range(n-1):
+        matrix[2*n+i][4*i:4*i+7] = [3*points[i+1][0]**2,2*points[i+1][0],1,0,-3*points[i+1][0]**2,-2*points[i+1][0],-1]
+
+    ## f1''(x2)-f2''(x2)=0
+    for i in range(n-1):
+        matrix[2*n+(n-1)+i][4*i:4*i+6] = [6*points[i+1][0],2,0,0,-6*points[i+1][0],-2]
+
+    ## f1''(x1)=0
+    matrix[-2][0:2]=[6*points[0][0],2]
+    ## f(n-1))''(x(n-1))=0
+    matrix[-1][-4:-2]=[6*points[-1][0],2]
+
+    inversematrix=np.linalg.inv(matrix)
+
+    print(matrix)
+    print(matb)
+    print(np.matmul(inversematrix,matb))
+    coefficientmatrix=np.matmul(inversematrix,matb)
+    pieces=[[points[i][0],points[i+1][0],coefficientmatrix[4*i][0],coefficientmatrix[4*i+1][0],coefficientmatrix[4*i+2][0],coefficientmatrix[4*i+3][0]]
+            for i in range(n)]
+    return pieces
+
 def integratecubic(x1,x2,a,b,c,d):
     return a*(x2**4-x1**4)/4 + b*(x2**3-x1**3)/3 + c*(x2**2-x1**2)/2+d*(x2-x1)
 
