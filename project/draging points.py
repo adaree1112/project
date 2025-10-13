e6f0f3ed7f91444fed6b845edecdf5637bac4d70
adam <adaree1112@highgateschool.org.uk>
2025-10-13 21:12:13 +0100

    cubic splines neatened

---
+++ project/draging points.py
@@ -0,0 +1,111 @@
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
+from project.piecewisecubicsplines import piecewise_cubic_spline,piecewise_linear
+import random
+
+class DraggablePoint:
+    def __init__(self, coord,refresh_callback):
+        self.x,self.y= coord
+        self.refresh_callback = refresh_callback
+        self.artist=None
+        self.press = None
+
+    def attach (self,ax):
+        self.artist,=ax.plot(self.x,self.y,'ro',picker=5)
+        self.connect()
+
+    def connect(self):
+        fig = self.artist.figure
+        self.cidpress = fig.canvas.mpl_connect('button_press_event', self.on_press)
+        self.cidrelease = fig.canvas.mpl_connect('button_release_event', self.on_release)
+        self.cidmotion = fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
+
+
+    def on_press(self, event):
+        if event.inaxes != self.artist.axes:
+            return
+        contains, _ = self.artist.contains(event)
+        if not contains:
+            return
+        x0, y0 = self.artist.get_data()
+        self.press = (x0[0], y0[0]), (event.xdata, event.ydata)
+
+    def on_motion(self, event):
+        if self.press is None or event.inaxes != self.artist.axes:
+            return
+        (x0, y0), (xpress, ypress) = self.press
+        dx = event.xdata - xpress
+        dy = event.ydata - ypress
+        self.x = x0 + dx
+        self.y = max(y0 + dy,0)
+        self.artist.set_data([self.x], [self.y])
+        self.artist.figure.canvas.draw()
+
+    def on_release(self, event):
+        self.press = None
+        self.artist.figure.canvas.draw()
+        self.refresh_callback()
+
+    def get_coords(self):
+        return self.x, self.y
+
+
+class PiecewiseGraph(tk.Frame):
+    def __init__(self, master,coords):
+        super().__init__(master)
+        self.fig = Figure(figsize = (5, 5), dpi = 100)
+        self.ax = self.fig.add_subplot()
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.get_tk_widget().pack()
+        self.canvas.draw()
+
+        self.points = [DraggablePoint(coord,self.refresh) for coord in coords]
+
+        self.refresh()
+
+    def add_point(self):
+        coords=[point.get_coords() for point in self.points]
+        currx,curry=zip(*coords)
+        self.points.append(DraggablePoint((random.uniform(min(currx),max(currx)),random.uniform(min(curry),max(curry))),self.refresh))
+        self.refresh()
+
+    def remove_point(self):
+        self.points.remove(random.choice(list(self.points)))
+        self.refresh()
+
+    def refresh(self):
+        self.ax.clear()
+
+        for point in self.points:
+            point.attach(self.ax)
+
+        coords=[point.get_coords() for point in self.points]
+        print(coords)
+        x,y=zip(*coords)
+        self.ax.plot(x,y,'ro')
+
+        self.pieces=piecewise_cubic_spline(coords)
+
+        for piece in self.pieces:
+            lower, upper, a, b, c, d, = piece
+            x = np.linspace(lower, upper, 50)
+            y = a * x ** 3 + b * x ** 2 + c * x + d
+
+            self.ax.plot(x, y)
+        self.ax.set_ylim(bottom=0)
+
+        self.canvas.draw()
+
+
+
+points=[(0,0),(1,2),(2,1.5)]
+pieces=[]
+
+root = tk.Tk()
+root.title("Adding points")
+graph_page = PiecewiseGraph(root,points)
+
+graph_page.pack()
+root.mainloop()
--- project/piecewisecubicsplines.py
+++ project/piecewisecubicsplines.py
@@ -1,6 +1,4 @@
-from matplotlib import pyplot as plt
-
-from quickplotpiecewisecubic import plotpieces
+import matplotlib.pyplot as plt
 
 import numpy as np
 
@@ -62,92 +60,91 @@ def piecewise_cubic_spline(points):
     matrix[-1][-3] = 2 * points[-1][0]
     matrix[-1][-2] = 1
 """
-    inversematrix=np.linalg.inv(matrix)
 
 
 
     # print(matrix)
     # print(matb)
     #print(np.matmul(inversematrix,matb))
-    coefficientmatrix=np.matmul(inversematrix,matb)
-
-    pieces=[[points[i][0],points[i+1][0],coefficientmatrix[4*i][0],coefficientmatrix[4*i+1][0],coefficientmatrix[4*i+2][0],coefficientmatrix[4*i+3][0]]
+    # inversematrix=np.linalg.inv(matrix)
+    # coefficientmatrix=np.matmul(inversematrix,matb)
+    coefficientmatrix=np.linalg.solve(matrix,matb)
+    pieces=[np.array([points[i][0],points[i+1][0],coefficientmatrix[4*i][0],coefficientmatrix[4*i+1][0],coefficientmatrix[4*i+2][0],coefficientmatrix[4*i+3][0]])
             for i in range(n)]
-
+    pieces=dealwithnegatives(pieces)
     return pieces
 
-def findrootsandfix(piece):
-    x1,x2,a,b,c,d=piece
-    roots = sorted([root for root in np.roots(piece[2:]) if np.isreal(root) and x1 <= root <= x2])
-    p=[x1]+roots+[x2]
-    newpieces=[]
-    for x1,x2 in zip(p,p[1:]):
-        x=(x1+x2)/2
-        y=a*x**3+b*x**2+c*x+d
-        if y<0:
-            newpieces.append([x1,x2,0,0,0,0])
-        else:
-            newpieces.append([x1,x2,a,b,c,d])
-    return newpieces
+def piecewise_linear(points):
+    points = sorted(points, key=lambda p: p[0])
 
+    output = []
+    for point, nextpoint in zip(points, points[1:]):
+        if point == nextpoint:
+            continue
+        x1, y1 = point
+        x2, y2 = nextpoint
+        c = (y2 - y1) / (x2 - x1)
+        d = y1 - c * x1
+        output.append(np.array([x1, x2, 0, 0, c,d]))
+    return output
 
-
-def deal_with_negatives(pieces):
+def dealwithnegatives(pieces):
     newpieces=[]
     for piece in pieces:
         x1,x2,a,b,c,d=piece
         x=np.linspace(x1,x2,100)
         y=a*x**3+b*x**2+c*x+d
         if min(y)<0:
-            newpieces+=findrootsandfix(piece)
+
+            roots = sorted([root for root in np.roots(piece[2:]) if np.isreal(root) and x1 <= root <= x2])
+            p = [x1] + roots + [x2]
+            for x1, x2 in zip(p, p[1:]):
+                x = (x1 + x2) / 2
+                y = a * x ** 3 + b * x ** 2 + c * x + d
+                if y < 0:
+                    newpieces.append(np.array([x1, x2, 0, 0, 0, 0]))
+                else:
+                    newpieces.append(np.array([x1, x2, a, b, c, d]))
         else:
             newpieces.append(piece)
     return newpieces
 
-def integratecubic(x1,x2,a,b,c,d):
-    return a*(x2**4-x1**4)/4 + b*(x2**3-x1**3)/3 + c*(x2**2-x1**2)/2+d*(x2-x1)
-
 def integratepiecewisecubic(pieces):
     area=0
     for piece in pieces:
-        area+=integratecubic(*piece)
+        x1,x2,a,b,c,d=piece
+        area+=a*(x2**4-x1**4)/4 + b*(x2**3-x1**3)/3 + c*(x2**2-x1**2)/2+d*(x2-x1)
     return area
 
-def normalisepiecewisecubic(pieces,fpieces):
-
-    points=[]
-    for piece in pieces:
-        x1=piece[0]
-        x2=piece[1]
-        a=piece[2]
-        b=piece[3]
-        c=piece[4]
-        d=piece[5]
-
-        y1=a*x1**3+b*x1**2+c*x1+d
-        y2=a*x2**3+b*x2**2+c*x2+d
-        points+=[(x1,y1)]
-    points+=[(x2,y2)]
-
-    print(points)
-
-    area=integratepiecewisecubic(fpieces)
-    areascalefactor=1/area
-
-    points=[(point[0],areascalefactor*point[1])for point in points]
-    pieces=piecewise_cubic_spline(points)
-    pieces=deal_with_negatives(pieces)
+def normalisepiecewisecubic(points,pieces):
+    area=integratepiecewisecubic(pieces)
+    k=1/area
+    points=[(point[0],k*point[1]) for point in points]
+    pieces=[np.concatenate((piece[:2],k * piece[2:])) for piece in pieces]
     return points,pieces
 
+def plotpieces(points,pieces):
+    for point in points:
+        plt.plot(point[0], point[1], 'ro')
 
-points=[(1,0), (2, 0.25), (3, 2), (4, 1)]
+    for piece in pieces:
+        lower,upper,a,b,c,d,=piece
+        x=np.linspace(lower,upper,50)
+        #y=np.maximum(a*x**3 + b*x**2 + c*x + d,0)
+        y=a*x**3 + b*x**2 + c*x + d
 
-# #print(piecewise_cubic_spline(points))
-pieces=piecewise_cubic_spline(points)
-fpieces=deal_with_negatives(pieces)
-plotpieces(points,fpieces)
+        plt.plot(x,y)
+    plt.show()
 
-# pieces=[[2,3,-0.75,4.5,-6.25,3.5],[3,4,0.75,-9,34.25,-37.0]]
-points,pieces=normalisepiecewisecubic(pieces,fpieces)
-plotpieces(points,pieces)
-print(integratepiecewisecubic(pieces))
+
+if __name__=='__main__':
+    points=[(1,0), (2, 0.25), (3, 2), (4, 1)]
+
+    #print(piecewise_cubic_spline(points))
+    pieces=piecewise_cubic_spline(points)#+piecewise_linear(points)
+    #pieces=piecewise_linear(points)
+    plotpieces(points,pieces)
+
+    points,pieces=normalisepiecewisecubic(points,pieces)
+    plotpieces(points,pieces)
+    print(integratepiecewisecubic(pieces))
